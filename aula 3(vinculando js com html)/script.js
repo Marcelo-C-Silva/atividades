04cb7fb8cdd9bb05dea13cb8db2f6fc5db05d4d4
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-26 15:30:38 -0300

    html vinculado com JS

---
+++ aula 3(vinculando js com html)/script.js
@@ -0,0 +1,2 @@
+console.log("marmininu");
+//lembrando que tudo dentro do console.log so aparece no console, então entre no arquivo html,( na qual esse script está interligado),usando um navegador aperte F12 no navegador e olhe no console, lá vai aparecer//
