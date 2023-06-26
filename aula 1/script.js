82ef9678a5116d42fe3f1f38d8ed47c8bacefc9f
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-26 14:33:44 -0300

    anotando a capacidade do console.log

--- aula 1/script.js
+++ aula 1/script.js
@@ -1,3 +1,7 @@
 console.log("João 'Marcelo'");// string = texto
 console.log(`João "Marcelo"`);// string = texto
 console.log('João `Marcelo`');// string = texto
+
+console.log(53, 15.85);// Number = Número ( esse tipo de dado sempre vai ser number)
+
+//console.log é uma função usada para exibir alguma coisa, ele somente vai ser exibido dentro do console//
