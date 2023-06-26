7a78ba6d5cb1b72652d575245c952b61911db44e
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-26 16:00:12 -0300

    variavel let

---
+++ aula 4(variaveis com let)/script.js
@@ -0,0 +1,14 @@
+let father = 'João';//string, pois esta entre aspas.
+let mother = 'Maria';
+let son = 'Eduardo';
+
+console.log(father,'nasceu em 1984.');
+console.log('Em 2000',father,'conheceu Maria.');
+console.log(father,'casou-se com Maria em 2012.');
+console.log(mother,'teve 1 filho com',father,'em 2015.');
+console.log('O filho de',father, 'se chama',son,'.');
+
+//Declarando as variaveis "let", e colocando elas em frases, a qualquer momento eu posso altera-las no codigo//
+//Otimo para usar quando tiver algumas repetições do codigo//
+//Não podemos criar variaveis com linguagens reservadas, tipo( let console = 'joão'; let let = 'joão'; let var = 'joão';)//
+//Evite tambem de colocar nomes aleatorios, tipo (let n = 'astoufo';)//
