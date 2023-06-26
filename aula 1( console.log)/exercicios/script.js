5e406192c3b2728bec2a8f47831557c6e7f86ee9
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-26 14:43:07 -0300

    ex

---
+++ aula 1( console.log)/exercicios/script.js
@@ -0,0 +1,3 @@
+console.log("Meu nome é 'João'. Estou aprendendo javaScript ás",10,"da manhã.")
+
+//sempre que for colocar um "number" com uma "string" dentro de um console.log, tem que colocar fora das aspas e usar vírgula//
