acf4c0634fbe1be5ae26909d17a0f86785ec048c
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-26 14:50:31 -0300

    comentarios

---
+++ aula 2 (comentando codigo)/index.js
@@ -0,0 +1,6 @@
+// Comentarios sempre vão ser ignorados, usa-se entre duas barras(//)//
+//Um exemplo é que tudo que foi escrito entre essas duas barras não aparecerá no console, OLHA SÒ//
+console.log('Não estou vendo');
+//viu, não aparece//
+console.log('doidera d+ mermão');
+//so ignorando//
