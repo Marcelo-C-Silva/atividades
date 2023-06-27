affc828a8e410870e94a6adddfb1ce9fa1dea40d
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-27 08:21:44 -0300

    const

--- aula 5( constantes)/script.js
+++ aula 5( constantes)/script.js
@@ -1,5 +1,12 @@
+//as mesmas regras do let se aplica a const, mas não podemos alterar uma const no meio do código,afinal, uma const é uma constante//
 const nome = 'João';
 console.log(nome);
 
-//as mesmas regras do let se aplica a const, mas não podemos alterar uma const no meio do código,afinal, uma const é uma constante//
+//um exemplo usando const com calculo//
+const primeiroNumero = 3;
+const segundoNumero = 5;
+const resultado = primeiroNumero * segundoNumero;
+//enves de modificar o valor dela, podemos acrescentar para evoluir o valor//
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
 
