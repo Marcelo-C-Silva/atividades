575236cb60c78ed29cedf2551ba9c4afa519c13f
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-27 13:01:30 -0300

    dados primarios

--- aula 5( constantes)/exercicio/script.js
+++ aula 5( constantes)/exercicio/script.js
@@ -1,14 +1,15 @@
-let nome ='Luiz Otávio';
-let sobreNome ='Miranda';
+let nome = 'Luiz Otávio';
+let sobreNome = 'Miranda';
 let idade = 30;
-let peso= 84;
-let altura=1.80;
+let peso = 84;
+let altura = 1.80;
 let imc;// peso / (altura * altura)
 let anoNascimento;
 
-imc = peso/(altura * altura);
+imc = peso / (altura * altura);
 anoNascimento = 2023 - idade;
 
 console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso}kg, tem ${altura} de altura e seu imc é de ${imc}.`);
 console.log(`${nome}, nasceu em ${anoNascimento}.`)
 //Luiz Otávio Miranda tem 30 anos,pesa 84 kg, tem 1.8 de altura e seu imc é de 25.925925925925924 Luiz Otávio nasceu em 1993//
+//usar sempre esse macete do `${}`//
---
+++ aula 6(dados primitivos)/script.js
@@ -0,0 +1,7 @@
+const nome = 'jose';//string//
+const nome2 = "jose";//string//
+const nome3 = `jose`;//string//
+const num = 10;//number//
+const num2 = 10.52;//number//
+let nomeAluno;// undefined:não aponta local na memoria//
+let sobrenomeAluno = null; // undefined:não aponta local na memoria//
