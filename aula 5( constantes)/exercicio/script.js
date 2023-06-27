b7f0867e07917543f4e05235debdab974c2b8c7b
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-27 09:05:04 -0300

    ajeitando

--- aula 5( constantes)/exercicio/script.js
+++ aula 5( constantes)/exercicio/script.js
@@ -7,8 +7,8 @@ let imc;// peso / (altura * altura)
 let anoNascimento;
 
 imc = peso/(altura * altura);
-anoNascimento = 2023 - 1980;
+anoNascimento = 2023 - idade;
 
 console.log(nome, sobreNome ,'tem',idade,'anos,pesa',peso,'kg, tem',altura,'de altura e seu imc é de',imc,);
-console.log(nome,'nasceu em 1980, e sua idade é',anoNascimento,'anos.')
-//Luiz Otávio Miranda tem 30 anos,pesa 84 kg, tem 1.8 de altura e seu imc é de 25.925925925925924 Luiz Otávio nasceu em 1980, e sua idade é 43 anos.//
+console.log(nome,'nasceu em',anoNascimento)
+//Luiz Otávio Miranda tem 30 anos,pesa 84 kg, tem 1.8 de altura e seu imc é de 25.925925925925924 Luiz Otávio nasceu em 1993//
