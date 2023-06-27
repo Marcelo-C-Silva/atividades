58f8d945582606e0a204bc5da46b348b4d4abff7
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-27 11:26:01 -0300

    refatorando

--- aula 5( constantes)/exercicio/script.js
+++ aula 5( constantes)/exercicio/script.js
@@ -9,6 +9,6 @@ let anoNascimento;
 imc = peso/(altura * altura);
 anoNascimento = 2023 - idade;
 
-console.log(nome, sobreNome ,'tem',idade,'anos,pesa',peso,'kg, tem',altura,'de altura e seu imc é de',imc,);
-console.log(nome,'nasceu em',anoNascimento)
+console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso}kg, tem ${altura} de altura e seu imc é de ${imc}.`);
+console.log(`${nome}, nasceu em ${anoNascimento}.`)
 //Luiz Otávio Miranda tem 30 anos,pesa 84 kg, tem 1.8 de altura e seu imc é de 25.925925925925924 Luiz Otávio nasceu em 1993//
