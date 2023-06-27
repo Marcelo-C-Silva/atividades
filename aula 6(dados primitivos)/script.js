de227eaae692216743f8199a50fc369b49633035
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-27 13:18:34 -0300

    primitivos

--- aula 6(dados primitivos)/script.js
+++ aula 6(dados primitivos)/script.js
@@ -5,3 +5,4 @@ const num = 10;//number//
 const num2 = 10.52;//number//
 let nomeAluno;// undefined:não aponta local na memoria//
 let sobrenomeAluno = null; // undefined:não aponta local na memoria//
+const boolean = true; //so tem a possibilidade de dois valores (true, false)//
