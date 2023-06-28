bd5b1b984db9222e6bfab9ffd3f1d0bd890aef69
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-28 08:52:38 -0300

    operadores

---
+++ aula7(operadores aritmeticos, de atribuição e incremento)/script.js
@@ -0,0 +1,23 @@
+//quando usar o "+" com number, vou ter o resultado de adição//
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+//resultado = 15//
+
+//agora se colocar uma string, vou ter o resultado de concatenação//
+const num3 = '5';
+const num4 = 10;
+console.log(num1 + num2);
+//resultado = 510//
+
+// ** dois asteriscos significa que você quer potencializar//
+const num5 = 2;
+const num6 = 10;
+console.log(num5 ** num6);
+//resultado = 1024//
+
+// % significa resto de divisão//
+const num7 = 10;
+const num8 = 3;
+console.log(num7 % num8);
+//reultado = 1//
