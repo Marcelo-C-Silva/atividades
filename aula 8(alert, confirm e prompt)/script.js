bdc439bad19a1598c894c16a72cb61bc3406fab5
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-28 10:22:45 -0300

    confirm

--- aula 8(alert, confirm e prompt)/script.js
+++ aula 8(alert, confirm e prompt)/script.js
@@ -1,1 +1,5 @@
 alert('eae men?');
+//alert é um atalho para window.alert()//
+
+confirm('tem certeza?');
+//vai retornar um valor boolean, se eu clicar 'ok' ele retornará 'true' e se eu clicar em cancelar, vai retornar 'false'//
