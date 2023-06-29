780b41f438cc654c35ebd0a61ef373c7d8cce2f6
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-29 10:44:09 -0300

    adicionando outro metodo de resolução

--- aula 4(variaveis com let)/ex/script.js
+++ aula 4(variaveis com let)/ex/script.js
@@ -8,3 +8,5 @@ varB = varC
 varC = intruso;
 console.log(varA, varB, varC);
 //quando tiver um problema desse, basta criar uma nova variavel e atribuir outra variavel como valor, assim o numero não se repete//
+
+//podemos resolver tbm assim [varA, varB, varC] = [varB, varC, varA]//
