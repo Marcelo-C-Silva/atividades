f91335d919982666d64f851a7d4fa7fb5d159d6a
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-29 10:46:07 -0300

    anotação

--- aula 4(variaveis com let)/ex/script.js
+++ aula 4(variaveis com let)/ex/script.js
@@ -2,11 +2,14 @@ let varA = 'A';
 let varB = 'B';
 let varC = 'C';
 let intruso = varA;
-
+//quando tiver um problema desse, basta criar uma nova variavel e atribuir outra variavel como valor, assim o numero não se repete//
 varA = varB
 varB = varC
 varC = intruso;
 console.log(varA, varB, varC);
-//quando tiver um problema desse, basta criar uma nova variavel e atribuir outra variavel como valor, assim o numero não se repete//
+
 
 //podemos resolver tbm assim [varA, varB, varC] = [varB, varC, varA]//
+[varA, varB, varC] = [varB, varC, varA]
+console.log(varA, varB, varC);
+
