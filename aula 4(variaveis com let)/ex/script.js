49350a892f2eb10fbc41ee3f1081cfb12f7ca772
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-29 10:35:59 -0300

    resolvendo logica

---
+++ aula 4(variaveis com let)/ex/script.js
@@ -0,0 +1,10 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+let intruso = varA;
+
+varA = varB
+varB = varC
+varC = intruso;
+console.log(varA, varB, varC);
+//quando tiver um problema desse, basta criar uma nova variavel e atribuir outra variavel como valor, assim o numero não se repete//
