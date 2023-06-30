ee0002f7322acf541841a84059c6e6e18e18d493
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-30 09:29:14 -0300

    complementando

--- aula 9(mais sobre strings)/script.js
+++ aula 9(mais sobre strings)/script.js
@@ -6,3 +6,11 @@ console.log(umaString);
 //indice 0 eh a primeira posição)
 console.log(umaString[4]);
 //resultado vai dar E, pois no scopo o E ta na quarta posição//
+
+//podemos usar indexOf() para saber a posição//
+console.log(umaString.indexOf('o', 3));
+//vai procurar O a partir da posição 3, caso ta na setima posição//
+
+//usamos lastIndexOf() para vim procurando de trás para frente//
+console.log(umaString.lastIndexOf('m', 3));
+//no caso, M ta na primeira posição//
