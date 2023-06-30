e4868281111725288291fa2bfd6a9ba39244e081
Marcelo <joao69@aluno.senai-ce.org.br>
2023-06-30 09:14:54 -0300

    mais sobre strings

---
+++ aula 9(mais sobre strings)/script.js
@@ -0,0 +1,8 @@
+// para escapar um caractere basta so colocar uma \ //
+let umaString = 'um texto';
+
+console.log(umaString);
+
+//indice 0 eh a primeira posição)
+console.log(umaString[4]);
+//resultado vai dar E, pois no scopo o E ta na quarta posição//
