c4c5807bb47dcc66fae82388de4ebe271257e6c5
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-02 18:38:27 -0300

    toFixed()

--- aula 10( mais sobre numbers)/script.js
+++ aula 10( mais sobre numbers)/script.js
@@ -1,5 +1,16 @@
-let num1 = 1 ;
+let num1 = 10.5657657665476546;
 let num2 = 2.5;
 
 //convertendo numero para string com toString()//
 console.log(num1.toString()+num2);
+//se eu quiser alterar de modo "permante" number para string com toString//
+//---------> num1 = num1.toString();<-----------------//
+
+//se caso eu queira ver a representação binaria do number com toString//
+console.log(num1.toString(2));
+
+//para numeros com cvarias casas decimais, usar toFixed()//
+console.log(num1.toFixed(2));
+//o 2 representa o quanto de casas decimais eu queira mostrar, no caso duas//
+
+
