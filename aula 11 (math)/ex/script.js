566eb9f92f20727e94ca55b7762f83b3b401caa7
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-03 11:44:33 -0300

    getElementById

---
+++ aula 11 (math)/ex/script.js
@@ -0,0 +1,6 @@
+let numero = prompt('Digite o Número:');
+
+//para selecionar o id de algum elemento basta usar o metodo getElementById()
+//dentro dos paranteses voce coloca o id que você quer selecionar
+//tem q salvar em algum canto usando uma variavel
+let numeroTItulo = document.getElementById('numero-titulo');
