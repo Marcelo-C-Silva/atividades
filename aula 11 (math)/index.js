43a7ad79d67e15f86ef6b00db3605bf960f1fb7e
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-03 09:09:36 -0300

    math

---
+++ aula 11 (math)/index.js
@@ -0,0 +1,23 @@
+let num1 = 9.54546;
+// let num2 = Math.floor(num1);
+//uso Math.floor() para arredondar para baixo, no caso vai da 9//
+// console.log(num2);
+
+//agora se eu quiser arredondar pra cima eu uso Math.ceil()//
+// let num2 = Math.ceil(num1);
+// console.log(num2);
+
+
+//e se eu quiser arredondar pro numero mais proximo, eu uso Math.round();
+// let num2 = Math.round(num1);
+// console.log(num2);
+
+
+//se eu quiser pegar o maior numero de uma sequencia eu uso Math.max()//
+console.log(Math.max(1,2,3,4,5,56,333456,132323,9888876));
+//agora se eu quiser pegar o menor valor de uma sequencia eu uso o Math.min()//
+console.log(Math.min(1,2,3,4,5,56,333456,132323,9888876));
+
+
+//se eu quiser gerar numeros aleatorios eu uso o Math.radom()//
+console.log(Math.random());
