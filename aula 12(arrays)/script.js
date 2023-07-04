b44d621a89c1b252eca7410f02af51f0c59d9b01
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-04 11:16:38 -0300

    arrays

---
+++ aula 12(arrays)/script.js
@@ -0,0 +1,16 @@
+//array pode ser imaginado como uma lista de coisas//
+//array é envolto de colchete//
+let alunos = ['joao', 'maria', 'jose']
+console.log(alunos);
+//arrays tambem são indexados, mas  por elementos//
+//                0        1        2
+let alunosEx = ['joao', 'maria', 'jose']
+//então para rxibir somente 1, basta colocar o index dele//
+console.log(alunosEx[0]);
+
+//caso eu queira editar, basta eu fazer o seguinte//
+alunosEx[0] = 'Luizin';
+console.log(alunosEx);
+//veja que luizin substituiu joao//
+
+
