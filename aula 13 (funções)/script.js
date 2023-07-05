a12433d1a000b62dc69aa311130e9b7b581807c6
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-05 09:26:18 -0300

    deixando mais legivel

--- aula 13 (funções)/script.js
+++ aula 13 (funções)/script.js
@@ -1,5 +1,6 @@
-function saudacao(nome) {
-    console.log(`Bom dia ${nome}!`);   
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;   
 }
 
-saudacao('Carlos');
+console.log(soma(5, 10));
