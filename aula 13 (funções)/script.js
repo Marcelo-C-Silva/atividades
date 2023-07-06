01ddb1908da17821426c865411a5735d686245e8
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-06 14:01:04 -0300

    arrow function

--- aula 13 (funções)/script.js
+++ aula 13 (funções)/script.js
@@ -1,6 +1,7 @@
-function soma(x, y) {
-    const resultado = x + y;
-    return resultado;   
-}
+//ARROW FUNCTION
 
-console.log(soma(5, 10));
+let raiz = (n) => {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
