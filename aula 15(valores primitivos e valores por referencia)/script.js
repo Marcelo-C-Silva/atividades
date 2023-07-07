4c085f12ec7a247dcc7ff94fd30b992ccf4c166f
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-07 10:57:33 -0300

    push

--- aula 15(valores primitivos e valores por referencia)/script.js
+++ aula 15(valores primitivos e valores por referencia)/script.js
@@ -1,9 +1,26 @@
 function meuEscopo() {
-    const form = document.querySelector('.form');
+    let form = document.querySelector('.form');
+    let resultado = document.querySelector('.resultado');
+
+    let pessoas = [];
 
     function recebeEventoForm(evento) {
         evento.preventDefault();
+        let nome = form.querySelector('.nome');
+        let sobrenome = form.querySelector('.sobrenome');
+        let peso = form.querySelector('.peso')
+        let altura = form.querySelector('.altura')
+        
 
+        pessoas.push({ //Quando eu quiser preencher um form com array eu uso isso
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
     };
     form.addEventListener('submit', recebeEventoForm);
 }
+meuEscopo();
+
+nome.value
