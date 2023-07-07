3744092dd68056d2ccee0066cff84a59ec442d50
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-07 14:08:52 -0300

    logica de programação

---
+++ aula 16(operadores de comparação)/script.js
@@ -0,0 +1,11 @@
+// operadores de comparação
+// > maior que
+// < menor que 
+// >= maior que ou igual a
+// <= menor que ou igual a 
+// == igualdade
+// === igualdade estrita
+// != diferente
+// !== diferente estrito
+
+console.log (10 > 5);
