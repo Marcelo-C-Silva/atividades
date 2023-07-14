685a4e752f27e2fcd6d7bdfd90c3ec5a5ba274e8
Marcelo <joao69@aluno.senai-ce.org.br>
2023-07-14 16:54:03 -0300

    if, else, else if

--- aula 18 (if, else if, else)/script.js
+++ aula 18 (if, else if, else)/script.js
@@ -1,8 +1,15 @@
-const caso1 = 500;
-const caso2 = 90;
+const hora = 10;
 
-if (caso1 < caso2) {
-    console.log('bora bill');
-}else{
-    console.log('não bora bill');
+if (hora >= 0 && hora <=11) {
+    console.log('bom dia!')
+}else if (hora >= 12 && hora <= 17) {
+    console.log('boa tarde!');
+} else if (hora >= 16 && hora <= 23) {
+    console.log('boa noite!')
+} else {
+    console.log('olá!')
 }
+// else e else if vem somente depois do if//   
+//posso ter varios else if na checagem//
+//eu so posso ter um else na checagem//
+//em alguns casos eu posso somente usar if//
