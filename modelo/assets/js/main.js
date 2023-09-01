20600d6284c8561f3fe458b92d1b2a9185b26572
Marcelo <joao69@aluno.senai-ce.org.br>
2023-09-01 13:47:09 -0300

    colocando html dentro da div

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -6,4 +6,5 @@ form.addEventListener('submit', function (event) {
 
 function setResultado (mensage) {
     let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = mensage;
 };
